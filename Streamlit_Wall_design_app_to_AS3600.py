434d4933d01a74a88cfdf59a8919da5153216aa9
mannyodi09 <EMMANUEL.ODI09@GMAIL.COM>
2023-12-04 16:48:51 +1000

    Stopped at df_combined3

--- Streamlit_Wall_design_app_to_AS3600.py
+++ Streamlit_Wall_design_app_to_AS3600.py
@@ -117,12 +117,35 @@ v2_dtype = v2_max.sort_index()
 st.dataframe(v2_dtype)
 
 #MERGE DATAFRAMES
+p_tens_ = p_tens_dtype.drop(["Case Type","Step Type","Location"], axis=1)
+p_compr_ = p_compr_dtype.drop(["Case Type","Step Type","Location"], axis=1)
+m3_ = m3_dtype.drop(["Case Type","Step Type","Location"], axis=1)
+v2_ = v2_dtype.drop(["Case Type","Step Type","Location"], axis=1)
+tens_compr_merge = p_compr_.merge(p_tens_,on=["Story","Pier"],how='outer')
+tens_compr_merge.index = tens_compr_merge.index.astype(story_categorical)
+p_t_merged = tens_compr_merge.sort_index()
 
+p_t_m3 = p_t_merged.merge(m3_,on=["Story","Pier"],how='outer')
+st.dataframe(p_t_merged)
 
+p_t_m3_v2 = p_t_m3.merge(v2_,on=["Story","Pier"],how='outer', suffixes=('_x1','_y1'))
+df_combined = p_t_m3_v2
+st.dataframe(df_combined)
 
+##Story heights
+story_prop=df_story_height2
+story_properties = story_prop.iloc[:-1]
+story_prop3=story_properties.rename(columns={"Name":"Story"})
+story_prop4=story_prop3.set_index("Story")
+st.dataframe(story_prop4)
+df_combined2 = df_combined.merge(story_prop4,on=["Story"],how='outer')
+st.dataframe(df_combined2)
 
-
-
-
-
+##Sort index
+st.dataframe(df_pier_props)
+df_pier_prop = df_pier_props.drop([0], axis=0)
+pier_props = df_pier_prop.drop(['AxisAngle','# Area Objects','# Line Objects','Width Top','Thickness Top','Material','CG Bottom X','CG Bottom Y','CG Bottom Z','CG Top X','CG Top Y','CG Top Z'], axis=1)
+pier_props1=pier_props.set_index("Story")
+df_combined3 = df_combined2.merge(pier_props,on=["Story","Pier"],how='outer')
+st.dataframe(df_combined3)
 
